e28775670f1f6346e3e65bea35c7bc8cef829e0a
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-03-22 19:57:54 +0530

    Working master worker

---
+++ master.py
@@ -0,0 +1,57 @@
+import os
+import json
+import requests
+import time
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import asyncio
+# Load environment variables
+load_dotenv()
+
+app = Flask(__name__)
+
+DB_LOCK = asyncio.Lock()
+
+@app.route('/save_data', methods=['GET'])
+def save_data():
+    payload = request.json
+    asyncio.run(save_data_async(payload))
+    
+    return jsonify({"message": "Data received", "payload": payload})
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"message": "Data received"})
+
+async def save_data_async(payload):
+    event_id = payload['eventId']
+    timestamp = payload['timestamp']
+    
+    # Create data directory if it doesn't exist
+    os.makedirs("data", exist_ok=True)
+    
+    # Create directory for event if it doesn't exist
+    event_dir = os.path.join("data", f"event_{event_id}")
+    os.makedirs(event_dir, exist_ok=True)
+    print(payload["available_qty"])
+    async with DB_LOCK: 
+        # Save data to event-specific files
+        yes_file = os.path.join(event_dir, "db_yes.csv")
+        no_file = os.path.join(event_dir, "db_no.csv")
+        
+        with open(yes_file, "a") as f:
+            f.write(f"{timestamp},{transform_data(payload['available_qty']['buy'])}\n")
+        with open(no_file, "a") as f:
+            f.write(f"{timestamp},{transform_data(payload['available_qty']['sell'])}\n")
+
+def transform_data(book):
+    keys = [i/2 for i in range(1,20)]
+    transformed_book = ""
+    for key in keys:
+        if str(key) in book:
+            transformed_book += f"{book[str(key)]},"
+    print(transformed_book)
+    return transformed_book
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ worker.py
@@ -0,0 +1,47 @@
+import os
+import requests
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HEADERS = {
+    "accept": "*/*",
+    "accept-language": "en",
+    "appid": "in.probo.pro",
+    "authorization": f"Bearer {os.getenv('PROBO_BEARER_TOKEN')}",
+    "content-type": "application/json"
+}
+
+PROBO_URL = "https://prod.api.probo.in/api/v3/tms/trade/bestAvailablePrice?eventId={eventId}"
+MASTER_URL = "http://127.0.0.1:5000/save_data"
+
+def get_available_qty( eventId: int ):
+    
+    response = requests.get( PROBO_URL.format( eventId = eventId ), headers = HEADERS )
+    
+    response.raise_for_status()
+    
+    data = response.json()
+    
+    return data["data"]["available_qty"]
+
+def send_data( available_qty: int, eventId: int ):
+
+    response = requests.get( 
+        MASTER_URL, 
+        headers = HEADERS, 
+        json = { 
+            "available_qty": available_qty,
+            "eventId": eventId,
+            "timestamp": time.time()
+        } 
+    )
+    response.raise_for_status()
+
+if __name__ == "__main__":
+    
+    EVENT_ID = 3824790
+
+    available_qty = get_available_qty( eventId = EVENT_ID )
+    send_data( available_qty, eventId = EVENT_ID )
